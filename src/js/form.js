114b0e0c216b4f8b61d96ca5f4c27457eab302e1
Eugen-Amoah <amoaheugen277@gmail.com>
2025-10-07 09:52:51 +0200

    modifiche

--- src/js/form.js
+++ src/js/form.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const addIssueBtn = document.getElementById("add-issue-btn");
   const formContainer = document.getElementById("form");
   const btnCancel= document.getElementById("cancel-btn");
+  const countbacklog = document.getElementById("backlog-count");
+  const countprogress = document.getElementById("in-progress-count");
+  const countcomplete = document.getElementById("complete-count");
+  
 
   
 
@@ -9,10 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
   addIssueBtn.addEventListener("click", function () {
     formContainer.classList.remove("hidden");
   });
-  btnCancel.addEventListener("click", function(){
-    formContainer.classList.add("hidden")
-  })
-
+  btnCancel.addEventListener("click", function () {
+    formContainer.classList.add("hidden");
+  });
+  
+  
  
 
 });

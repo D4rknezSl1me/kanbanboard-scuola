d8a68da3729d5870cb49b78f261c457922bf3526
Merigo-Luca <lucamerigo0610@gmail.com>
2025-10-13 11:30:15 +0200

    stile 4

--- src/js/form.js
+++ src/js/form.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const addIssueBtn = document.getElementById("add-issue-btn");
   const formContainer = document.getElementById("form");
   const btnCancel= document.getElementById("cancel-btn");
+  const btnAdd = document.getElementById("issue-btn1")
   const countbacklog = document.getElementById("backlog-count");
   const countprogress = document.getElementById("in-progress-count");
   const countcomplete = document.getElementById("complete-count");
@@ -17,7 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   btnCancel.addEventListener("click", function () {
     formContainer.classList.add("hidden");
-    bg.classList.add("hidden");;
+    bg.classList.add("hidden");
+  });
+  btnAdd.addEventListener("click", function () {
+    bg.classList.add("hidden");
   });
   
   
